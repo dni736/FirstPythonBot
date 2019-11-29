ad1f327d35c127b93d5abc074f716d20bfb6d5b1
dni736 <dima.nikolishvili@gmail.com>
2019-11-29 18:39:05 +0400

    finalise

--- FirstPythonBot.py
+++ FirstPythonBot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from telebot import types
 import telebot;
 bot = telebot.TeleBot('817988164:AAEDUAowZiTowLVs9yl7gv31CgoX_QA0Zjg');
 
@@ -36,12 +37,26 @@ def get_surname(message):
 	bot.register_next_step_handler(message, get_age);
 
 def get_age(message):
-	global age;
-	while age == 0:
-		try:
-			 age = int(message.text)
-		except Exception:
-			 bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
-		     bot.send_message(message.from_user.id, 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?')
+    global age;
+    while age == 0: #проверяем что возраст изменился
+        try:
+             age = int(message.text) #проверяем, что возраст введен корректно
+        except Exception:
+             bot.send_message(message.from_user.id, 'Цифрами, пожалуйста');
+      keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
+      key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes'); #кнопка «Да»
+      keyboard.add(key_yes); #добавляем кнопку в клавиатуру
+      key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
+      keyboard.add(key_no);
+      question = 'Тебе '+str(age)+' лет, тебя зовут '+name+' '+surname+'?';
+      bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
+        .... #код сохранения данных, или их обработки
+        bot.send_message(call.message.chat.id, 'Запомню : )');
+    elif call.data == "no":
+         ... #переспрашиваем
 
 bot.polling(none_stop=True, interval=0)
