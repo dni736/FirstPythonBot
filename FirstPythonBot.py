415112f379163275bd0ef1623586b783b9a00edc
dni736 <dima.nikolishvili@gmail.com>
2019-11-29 18:49:44 +0400

    fix

--- FirstPythonBot.py
+++ FirstPythonBot.py
@@ -54,9 +54,8 @@ def get_age(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
-        .... #код сохранения данных, или их обработки
         bot.send_message(call.message.chat.id, 'Запомню : )');
     elif call.data == "no":
-         ... #переспрашиваем
+        bot.send_message(call.message.chat.id, 'Заново');
 
 bot.polling(none_stop=True, interval=0)
